9fb4adb4fc4a5eea730846286aa7dbf22a8ad756
root <root@vm-1.cloud.mos.meituan.local>
2013-12-06 00:54:29 +0800

    更新 x

--- node_init.sh
+++ node_init.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-version=10.0.22
+version=v10.0.22
 node=node-$version
 if [ ! -d $node ]; then
-    wget http://nodejs.org/dist/v${version}/${node}.tar.gz
+    wget http://nodejs.org/dist/${version}/${node}.tar.gz
     tar xvf ${node}.tar.gz
 fi
 cd ${node}
