a13841cdffe13a9a3add19d99207342a6b41ef85
root <root@vm-3.cloud.mos.meituan.local>
2013-12-13 19:44:17 +0800

    fix nginx conf /mongodb db logs dir

--- gcc_arm.sh
+++ gcc_arm.sh
@@ -1,3 +1,3 @@
-sudo add-apt-repository ppa:terry.guo/gcc-arm-embedded
-sudo apt-get update
-sudo apt-get install gcc-arm-none-eabi
+sudo add-apt-repository -y ppa:terry.guo/gcc-arm-embedded
+sudo apt-get -y update
+sudo apt-get -y install gcc-arm-none-eabi
